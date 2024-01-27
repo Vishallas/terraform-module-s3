c5b44c42e487d240a986195e771703598836913d
vishal <flickerflak@gmail.com>
2024-01-27 09:46:49 +0530

    refactor(output):output format change and bucket id add

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "bucket_url" {
+output "bucket_domain_name" {
   value       = aws_s3_bucket.this.bucket_domain_name
-  description = "The domain name of the bucket."
+  description = "URL of the bucket."
 }
 
 output "bucket_arn" {
   value       = aws_s3_bucket.this.arn
-  description = "The arn of the bucket."
+  description = "arn of the bucket."
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.this.id
+  description = "Name of the bucket."
 }
