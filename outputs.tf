84c753434931b0efab7edc1e877df5fa100c2424
vishal <flickerflak@gmail.com>
2024-01-27 10:22:40 +0530

    refactor(output): description modify

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_domain_name" {
   value       = aws_s3_bucket.this.bucket_domain_name
-  description = "URL of the bucket."
+  description = "The domain name of the bucket."
 }
 
 output "bucket_arn" {
   value       = aws_s3_bucket.this.arn
-  description = "arn of the bucket."
+  description = "The arn of the bucket."
 }
 
 output "bucket_id" {
   value = aws_s3_bucket.this.id
-  description = "Name of the bucket."
+  description = "The name of the bucket."
 }
